ed46418afbfa97f58fa2a5fb9ef5d28c1c235b9c
pikkukurkku <143491239+pikkukurkku@users.noreply.github.com>
2024-05-28 22:16:08 +0200

    v-for

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const app = Vue.createApp({
   data() {
     return {
       showBooks: true,
-      title: "The Final Empire",
-      author: "Brandon Sanderson",
-      age: 45,
+    //   title: "The Final Empire",
+    //   author: "Brandon Sanderson",
+    //   age: 45,
+    //   x: 0,
+    //   y: 0
+    books: [
+        { title: 'name of the wind', author: 'patrick rothfuss'},
+        { title: 'the way of kings', author: 'brandon sanderson'},
+        { title: 'the final empire', author: 'brandon sanderson'},
+    ]
     };
   },
   methods: {
@@ -17,6 +24,10 @@ const app = Vue.createApp({
         console.log(data)
       }
     },
+    handleMouseMove(e) {
+        this.x = e.offsetX
+        this.y = e.offsetY
+    }
   },
 });
 
